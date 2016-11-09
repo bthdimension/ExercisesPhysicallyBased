adb7ebb4825016ce555c750054366129843e6d9c
Henry Raymond <github@henryraymond.com>
2016-11-09 12:00:34 +0100

    Update! Jetzt mit Symmetrie!

--- ex3_henry/exercise.cpp
+++ ex3_henry/exercise.cpp
@@ -61,28 +61,34 @@ void ExCorrectVelocities(int _xRes, int _yRes, double _dt, const Array2d &_press
 	double dx = 1.0 / _xRes;
 
 	for (int y = 1; y < _yRes - 1; y++) {
-		for (int x = 1; x < _xRes - 1; x++) {
-			double xVelo = _xVelocity(x + 1, y);
-			xVelo -= _dt * (_pressure(x + 1, y) - _pressure(x, y)) / dx;
-			_xVelocity(x + 1, y) = xVelo;
+		for (int x = 1; x < _xRes; x++) {
+			double xVelo = _xVelocity(x, y);
+			xVelo -= _dt * (_pressure(x, y) - _pressure(x - 1, y)) / dx;
+			_xVelocity(x, y) = xVelo;
 		}
 	}
 
-	for (int y = 1; y < _yRes - 1; y++) {
+	for (int y = 1; y < _yRes; y++) {
 		for (int x = 1; x < _xRes - 1; x++) {
-			double yVelo = _yVelocity(x, y + 1);
-			yVelo -= _dt * (_pressure(x, y + 1) - _pressure(x, y)) / dx;
-			_yVelocity(x, y + 1) = yVelo;
+			double yVelo = _yVelocity(x, y);
+			yVelo -= _dt * (_pressure(x, y) - _pressure(x, y - 1)) / dx;
+			_yVelocity(x, y) = yVelo;
 		}
 	}
 }
 
 // Problem 3
 
-double ExAdvectValue(int _x, int _y, double _offsetX, double _offsetY, Array2d &_xVelocity, Array2d &_yVelocity, Array2d &_values, int _xRes, int _yRes, double _dt) {
+double ExAdvectValue(int _x, int _y, double _offsetX, double _offsetY, Array2d &_xVelocity, Array2d &_yVelocity, Array2d &_values, int _xRes, int _yRes, double _dt, int _startX, int _endX, int _startY, int _endY) {
 
 	double interpolVeloX = ((1.0 - _offsetX) * _xVelocity(_x, _y)) + (_offsetX * _xVelocity(_x + 1, _y));
+	if (_offsetY == 0.0) {
+		interpolVeloX = 0.5 * (interpolVeloX + ((1.0 - _offsetX) * _xVelocity(_x, _y - 1)) + (_offsetX * _xVelocity(_x + 1, _y - 1)));
+	}
 	double interpolVeloY = ((1.0 - _offsetY) * _yVelocity(_x, _y)) + (_offsetY * _yVelocity(_x, _y + 1));
+	if (_offsetX == 0.0) {
+		interpolVeloY = 0.5 * (interpolVeloY + ((1.0 - _offsetY) * _yVelocity(_x - 1, _y)) + (_offsetY * _yVelocity(_x - 1, _y + 1)));
+	}
 
 	double pX = (double)_x - (interpolVeloX * _dt) * (double)_xRes;
 	double pY = (double)_y - (interpolVeloY * _dt) * (double)_yRes;
@@ -93,21 +99,21 @@ double ExAdvectValue(int _x, int _y, double _offsetX, double _offsetY, Array2d &
 	double interpolOffsetX = pX - (double)interpolBaseX;
 	double interpolOffsetY = pY - (double)interpolBaseY;
 
-	if (interpolBaseX < 2) {
-		interpolBaseX = 0;
+	if (interpolBaseX < _startX) {
+		interpolBaseX = _startX;
 		interpolOffsetX = 0.0;
 	}
-	if (interpolBaseX > _xRes - 3) {
-		interpolBaseX = _xRes - 3;
-		interpolOffsetX = 0.0;
+	if (interpolBaseX > _endX) {
+		interpolBaseX = _endX;
+		interpolOffsetX = 1.0;
 	}
-	if (interpolBaseY < 2) {
-		interpolBaseY = 0;
+	if (interpolBaseY < _startY) {
+		interpolBaseY = _startY;
 		interpolOffsetY = 0.0;
 	}
-	if (interpolBaseY > _yRes - 3) {
-		interpolBaseY = _yRes - 3;
-		interpolOffsetY = 0.0;
+	if (interpolBaseY > _endY) {
+		interpolBaseY = _endY;
+		interpolOffsetY = 1.0;
 	}
 
 	double val = _values(interpolBaseX, interpolBaseY) * (1.0 - interpolOffsetX) * (1.0 - interpolOffsetY);
@@ -121,17 +127,17 @@ void ExAdvectWithSemiLagrange(int _xRes, int _yRes, double _dt, Array2d &_xVeloc
 
 	for (int y = 1; y < _yRes - 1; y++) {
 		for (int x = 1; x < _xRes; x++) {
-			_xVelocityTemp(x, y) = ExAdvectValue(x, y, 0.0, 0.5, _xVelocity, _yVelocity, _xVelocity, _xRes, _yRes, _dt);
+			_xVelocityTemp(x, y) = ExAdvectValue(x, y, 0.0, 0.5, _xVelocity, _yVelocity, _xVelocity, _xRes, _yRes, _dt, 2, _xRes - 3, 1, _yRes - 2);
 		}
 	}
 	for (int y = 1; y < _yRes; y++) {
 		for (int x = 1; x < _xRes - 1; x++) {
-			_yVelocityTemp(x, y) = ExAdvectValue(x, y, 0.5, 0.0, _xVelocity, _yVelocity, _yVelocity, _xRes, _yRes, _dt);
+			_yVelocityTemp(x, y) = ExAdvectValue(x, y, 0.5, 0.0, _xVelocity, _yVelocity, _yVelocity, _xRes, _yRes, _dt, 1, _xRes - 2, 2, _yRes - 3);
 		}
 	}
 	for (int y = 1; y < _yRes - 1; y++) {
 		for (int x = 1; x < _xRes - 1; x++) {
-			_densityTemp(x, y) = ExAdvectValue(x, y, 0.5, 0.5, _xVelocity, _yVelocity, _density, _xRes, _yRes, _dt);
+			_densityTemp(x, y) = ExAdvectValue(x, y, 0.5, 0.5, _xVelocity, _yVelocity, _density, _xRes, _yRes, _dt, 1, _xRes - 2, 1, _yRes - 2);
 		}
 	}
 
